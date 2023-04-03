bff640ccb1e18e1ca4b465b0f73ac4739b913341
chuducdoan <48355044+Ongnoiban2010@users.noreply.github.com>
2023-04-03 18:20:41 +0700

    add font

---
+++ MyApp/assets/fonts/index.ts
@@ -0,0 +1,11 @@
+const PoppinsThin = require('./Poppins-Thin.ttf');
+const PoppinsRegular = require('./Poppins-Regular.ttf');
+const PopinsMedium = require('./Poppins-Medium.ttf');
+const PoppinSemiBold = require('./Poppins-SemiBold.ttf');
+
+export {
+    PoppinsThin,
+    PoppinsRegular,
+    PoppinSemiBold,
+    PopinsMedium
+}
---
+++ MyApp/constant/index.ts
@@ -0,0 +1,21 @@
+export const fontSize = {
+    size30: "30",
+    size14: "14"
+}
+
+export const color = {
+    bgWhite: "#fff",
+    bgLight: "#f4f5f9",
+    bgGray: "#f5f5f5",
+
+    textGray: "#868889",
+    textBlack: "#000",
+    
+    textLink: "#407EC7",
+
+    border: "#EBEBEB",
+
+    primary: "#AEDC81",
+    primaryDark: "#6CC51D",
+    primaryLight: "#EBFFD7"
+}
